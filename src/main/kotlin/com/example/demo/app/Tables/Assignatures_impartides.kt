a7ae7b48bf0f9980cd755d13fb0689e4e71c56ac
Ricard <ricardpinzonsuller@gmail.com>
2021-04-28 13:27:24 +0200

    Afegits objectes de tipus taula faltants al commit anterior i afegida la seva clau primaria.

--- src/main/kotlin/com/example/demo/app/Tables/Assignatures_impartides.kt
+++ src/main/kotlin/com/example/demo/app/Tables/Assignatures_impartides.kt
@@ -4,6 +4,6 @@ import org.ktorm.schema.Table
 import org.ktorm.schema.int
 
 object Assignatures_impartides: Table<Nothing>("assignatures_impartides") {
-    val id_professor = int("id_professor")
-    val id_modul = int("id_modul")
+    val id_professor = int("id_professor").primaryKey()
+    val id_modul = int("id_modul").primaryKey()
 }
