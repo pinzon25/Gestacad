4efaa5bb2ace9bb710b615adeecaf0dac85895c7
Reda <reda.ieslaslagunas@gmail.com>
2021-05-04 12:51:50 +0200

    Commit Profesors

--- src/main/kotlin/com/example/demo/app/Profesors.kt
+++ src/main/kotlin/com/example/demo/app/Profesors.kt
@@ -1,2 +1,21 @@
 package com.example.demo.app
 
+import javafx.beans.property.SimpleBooleanProperty
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
+import tornadofx.Controller
+import tornadofx.ItemViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+
+data class Profesors (var id:Int, var nom:String, var cognoms:String, var dni:String, var data_naixement: Date, var sexe:String, var telefon:String, var email:String, var descripcio:String){
+    val idProperty = SimpleIntegerProperty(id)
+    val nomProperty = SimpleStringProperty(nom)
+    val cognomsProperty = SimpleStringProperty(cognoms)
+    val dniProperty = SimpleStringProperty(dni)
+    val datanaixementProperty = SimpleStringProperty(data_naixement.toString())
+    val sexeProperty = SimpleStringProperty(sexe)
+    val telefonProperty = SimpleStringProperty(telefon)
+    val emailProperty = SimpleStringProperty(email)
+    val descripcioProperty = SimpleStringProperty(descripcio)
+}
