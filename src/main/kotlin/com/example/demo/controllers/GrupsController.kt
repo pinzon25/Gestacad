c9028aaa5e04baa7dc17575366aa8c8a984a2120
Ricard Pinzón <riki_net69@hotmail.com>
2021-05-10 21:32:34 +0200

    Add files via upload
    Implementat sistema de obtencio d'alumnes mitjançant el id del grup seleccionat.

--- src/main/kotlin/com/example/demo/controllers/GrupsController.kt
+++ src/main/kotlin/com/example/demo/controllers/GrupsController.kt
@@ -1,6 +1,8 @@
 package com.example.demo.controllers
 
+import com.example.demo.app.Alumne
 import com.example.demo.app.Connexio
+import com.example.demo.app.Tables.Alumne_grup
 import com.example.demo.app.model.Alumnes_grups
 import org.ktorm.dsl.*
 import tornadofx.Controller
@@ -8,7 +10,9 @@ import tornadofx.Controller
 class GrupsController : Controller(){
 
     var dd = Connexio().database
+    val alumneControler:AlumnesController by inject()
 
+    //Obtenim tots els grups
     fun obteGrups():MutableList<com.example.demo.app.Grups>{
         var grups:MutableList<com.example.demo.app.Grups> = ArrayList()
         var gr: com.example.demo.app.Grups?=null
@@ -24,6 +28,58 @@ class GrupsController : Controller(){
         }
 
         return grups
+    }
+
+
+    /*fun obteAlumnesIdGrup(idgrup:Int):MutableList<Alumne>{
+        var alumnesGrups:MutableList<Alumnes_grups> = ArrayList()
+        var alumnes:MutableList<Alumne> = ArrayList()
+        var ag: com.example.demo.app.model.Alumnes_grups?=null
+        for(row in dd!!.from(com.example.demo.app.Tables.Alumne_grup).select()){
+
+                val idGrup: Int? = row[com.example.demo.app.Tables.Alumne_grup.id_grup]
+                val idAlumne: Int? = row[com.example.demo.app.Tables.Alumne_grup.id_alumne]
+
+            if(idgrup == idGrup) {
+                alumnes = alumneControler.obteAlumnePerId(idAlumne!!)
+            }
+        }
+
+        return alumnes
+    }*/
+
+    fun obteAlumnesGrups(idGrup:Int):MutableList<Alumne>{
+        var alumnesGrups:MutableList<Alumnes_grups> = ArrayList()
+        var alumnes:MutableList<Alumne> = ArrayList()
+        var ag: com.example.demo.app.model.Alumnes_grups?=null
+
+        for(row in dd!!.from(com.example.demo.app.Tables.Alumne_grup).select()){
+
+            val idGrup:Int? = row[com.example.demo.app.Tables.Alumne_grup.id_grup]
+            val idAlumne:Int? = row[com.example.demo.app.Tables.Alumne_grup.id_alumne]
+
+            ag = com.example.demo.app.model.Alumnes_grups(idGrup!!,idAlumne!!)
+            alumnesGrups.add(ag) //Obtenim totes les dades de la taula grups_alumnes.
+        }
+
+
+        for(i in alumnesGrups.indices){
+            var alum:Alumne?=null
+            var idgrup:Int?=null
+            var idAlumne:Int?=null
+            idgrup = alumnesGrups.get(i).id_grup
+            //idAlumne = alumnesGrups.get(i).id_alumne
+            /*println("id del idgrup: " + idgrup) //Aqui ja obtenim el id del grup.
+            println("id del idalumne: " + idAlumne) //Aqui ja obtenim els id dels alumnes guardats al array.*/
+            if(idGrup == idgrup){
+                alumnes.add(alumneControler.obteAlumnePerId(alumnesGrups.get(i).id_alumne))
+            }
+
+
+        }
+
+        //println("Alumnes obtinguts mitjancant els id_alumne del array alumnesGrups: "+alumnes)
+        return alumnes
     }
 
     fun obteGrupsAlumne():MutableList<Alumnes_grups>{
