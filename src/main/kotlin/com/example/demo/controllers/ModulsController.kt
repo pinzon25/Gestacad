095d934d86bd878a2134293197937c7ff44ac692
Reda <reda.ieslaslagunas@gmail.com>
2021-05-11 16:17:42 +0200

    Commit Moduls

--- src/main/kotlin/com/example/demo/controllers/ModulsController.kt
+++ src/main/kotlin/com/example/demo/controllers/ModulsController.kt
@@ -14,10 +14,10 @@ class ModulsController  : Controller() {
         var mo: Moduls?=null
         for(row in dd!!.from(com.example.demo.app.Tables.Moduls).select()){
 
-            val id:Int? = row[com.example.demo.app.Tables.Grups.id]
-            val idcicle:Int? = row[com.example.demo.app.Tables.Grups.id_cicle]
-            val nom:String? = row[com.example.demo.app.Tables.Grups.nom]
-            val descripcio:String? = row[com.example.demo.app.Tables.Grups.descripcio]
+            val id:Int? = row[com.example.demo.app.Tables.Moduls.id]
+            val idcicle:Int? = row[com.example.demo.app.Tables.Moduls.id_cicle]
+            val nom:String? = row[com.example.demo.app.Tables.Moduls.nom]
+            val descripcio:String? = row[com.example.demo.app.Tables.Moduls.descripcio]
 
             mo = Moduls(id!!, idcicle!!, nom.toString(), descripcio.toString())
             moduls.add(mo)
