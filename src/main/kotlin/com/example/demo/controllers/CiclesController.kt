7371c2f6833dbc92a88aa024cc984addfb852191
BarberaThor <j.barberathor@gmail.com>
2021-05-18 19:54:53 +0200

    Controlador Cicles

--- src/main/kotlin/com/example/demo/controllers/CiclesController.kt
+++ src/main/kotlin/com/example/demo/controllers/CiclesController.kt
@@ -2,10 +2,12 @@ package com.example.demo.controllers
 
 import com.example.demo.view.Cicles
 import com.example.demo.app.Connexio
+import com.example.demo.app.Grups
 import com.example.demo.app.model.Cicle
 import org.ktorm.dsl.*
+import tornadofx.Controller
 
-class CiclesController {
+class CiclesController : Controller(){
 
     var dd = Connexio().database
 
@@ -27,4 +29,33 @@ class CiclesController {
 
         return cicles
     }
+
+    fun afegeixTaulaCicles(cicle: Cicle):Unit{
+        dd!!.insert(com.example.demo.app.Tables.Cicle) {
+            set(it.id, cicle.id)
+            set(it.id_familia, cicle.id_familia)
+            set(it.nom, cicle.nom)
+            set(it.descripcio, cicle.descripcio)
+            println("Has afegit correctament el grup!!!.")
+        }
+    }
+
+    //DELETE DE LA TAULA GRUPS.
+    fun esborraTaulaCicles(cicle: Cicle):Unit{
+        dd!!.delete(com.example.demo.app.Tables.Grups){
+            it.id eq cicle.id
+        }
+    }
+
+    fun actualitzarTaulaGrups(cicle: Cicle):Unit{
+        dd!!.update(com.example.demo.app.Tables.Grups){
+            set(it.id, cicle.id)
+            set(it.nom, cicle.nom)
+            set(it.descripcio, cicle.descripcio)
+            where { it.id eq cicle.id }
+        }
+    }
+
+
+
 }
