eea7946aa6ac80bb279d46e616d28e2464751681
Reda <reda.ieslaslagunas@gmail.com>
2021-05-23 17:02:26 +0200

    Controller Professors

--- src/main/kotlin/com/example/demo/controllers/ProfesorsController.kt
+++ src/main/kotlin/com/example/demo/controllers/ProfesorsController.kt
@@ -74,38 +74,6 @@ class ProfesorsController : Controller() {
         }
         return profesors
     }
-    fun obteProfesorsId(idProf: Int):Professor {
-        var profesors: MutableList<Professor> = ArrayList()
-        var prof: Professor? = null
-        for (row in dd!!.from(com.example.demo.app.Tables.Professor).select().where(com.example.demo.app.Tables.Professor.id eq idProf )) {
-
-            val id: Int? = row[com.example.demo.app.Tables.Professor.id]
-            val nom: String? = row[com.example.demo.app.Tables.Professor.nom]
-            val cognoms: String? = row[com.example.demo.app.Tables.Professor.cognoms]
-            val dni: String? = row[com.example.demo.app.Tables.Professor.dni]
-            val datanaixement: LocalDate? = row[com.example.demo.app.Tables.Professor.data_naixement]
-            val sexe: String? = row[com.example.demo.app.Tables.Professor.sexe]
-            val telefon: Int? = row[com.example.demo.app.Tables.Professor.telefon]
-            val email: String? = row[com.example.demo.app.Tables.Professor.email]
-            val descripcio: String? = row[com.example.demo.app.Tables.Professor.descripcio]
-
-            var d: Date = Date(datanaixement!!.year, datanaixement.monthValue, datanaixement.dayOfMonth)
-            prof = Professor(
-                id!!,
-                nom.toString(),
-                cognoms.toString(),
-                dni.toString(),
-                d,
-                sexe.toString(),
-                telefon.toString(),
-                email.toString(),
-                descripcio.toString()
-            )
-            profesors.add(prof)
-        }
-        return profesors.get(0)
-
-    }
     fun obteIdProfesorsMesGran():Int{
         var idMesGran:Int?=null
 
